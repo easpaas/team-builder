d693e3d492c32eaaa1c7b4cbb431c054dc83757a
evan <aspaasevan@gmail.com>
2020-03-25 12:22:38 -0700

    MVP done

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -2,37 +2,35 @@ import React, { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 
-function App() {
-  const [members, setMembers] = useState([
-    {
-      name: 'Dana Scully',
-      email: 'dscully@email.com', 
-      status: 'employed'
-    },
-    {
-      name: 'Fox Mulder',
-      email: 'mulder@email.com', 
-      status: 'employed'
-    },
-    {
-      name: 'Melvin Frohike',
-      email: 'frohike@email.com', 
-      status: 'unknown'
-    },
-    {
-      name: 'Richard Langly',
-      email: 'langly@email.com', 
-      status: 'unkonwn'
-    }
-  ]);
+const memberList = [
+  {
+    name: 'Dana Scully',
+    email: 'dscully@email.com', 
+    status: 'employed'
+  },
+  {
+    name: 'Melvin Frohike',
+    email: 'frohike@email.com', 
+    status: 'unknown'
+  },
+  {
+    name: 'Richard Langly',
+    email: 'langly@email.com', 
+    status: 'unknown'
+  }
+];
 
+function App() {
+  const [members, setMembers] = useState(memberList);
+ console.log(members)
   return (
     <div className='App'>
       <Form 
+        members={members}
         setMembers={setMembers}
       />
       <div className='list-container'>
-        <h2>Member List</h2>
+        <h2>MEMBER LIST</h2>
         {
           members.map((member, index) => {
             return <p key={index}>{member.name}</p>
--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import '../App.css';
 
-function Form({setMembers}) {
+function Form({members, setMembers}) {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -17,30 +17,37 @@ function Form({setMembers}) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setMembers(formData);
+    setMembers([
+      ...members,
+      formData
+    ]);
     setFormData({ 
       name: '',
       email: '',
       status: ''
     });
+  
   };
   
   return(
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name: 
-        <input type='text' name='name' value={formData.name} placeholder='full name' onChange={handleChange} />
-      </label>
-      <label>
-        Email: 
-        <input type='text' name='email' value={formData.email} placeholder='email' onChange={handleChange} />
-      </label>
-      <label>
-        Status: 
-        <input type='text' name='status' value={formData.status} placeholder='current status' onChange={handleChange} />
-      </label>
-      <input type='submit' value='Submit' />
-    </form>
+    <div className='form-container'>
+      <h2>ADD MEMBER</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name: 
+          <input type='text' name='name' value={formData.name} placeholder='full name' onChange={handleChange} />
+        </label>
+        <label>
+          Email: 
+          <input type='text' name='email' value={formData.email} placeholder='email' onChange={handleChange} />
+        </label>
+        <label>
+          Status: 
+          <input type='text' name='status' value={formData.status} placeholder='current status' onChange={handleChange} />
+        </label>
+        <input type='submit' value='Submit' />
+      </form>
+    </div>
   );
 }
 
